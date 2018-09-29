c06131990675b30a476bcec86ca526c13f9f76eb
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-28 23:46:56 -0300

    Implementing Product and Item models

--- src/main/kotlin/io/petproject/model/Item.kt
+++ src/main/kotlin/io/petproject/model/Item.kt
@@ -1,10 +1,27 @@
 package io.petproject.model
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 data class Item(val product: Product, val quantity: Int) {
-    val subtotal = product.price * quantity
+    val subtotal: BigDecimal = product.price
+            .multiply(quantity.toBigDecimal())
+
+    init {
+        require(quantity > 0) { "Quantity must be > 0" }
+    }
 }
 
-data class Product(val name: String, val type: ProductType, val price: Double)
+data class Product(val name: String, val type: ProductType, private val _price: Double) {
+    val price: BigDecimal = _price
+            .toBigDecimal()
+            .setScale(2, RoundingMode.HALF_UP)
+
+    init {
+        require(name.isNotBlank()) { "Product name cannot be blank" }
+        require(_price > 0.0) { "Produce price must be > 0" }
+    }
+}
 
 enum class ProductType {
     PHYSICAL,
