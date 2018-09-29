10f0dd42258cc16dbdf41d5cc891784104d1a0a4
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-29 02:49:51 -0300

    Updating test to add more products/items to the shopping cart

--- src/test/kotlin/io/petproject/model/ShoppingCartTest.kt
+++ src/test/kotlin/io/petproject/model/ShoppingCartTest.kt
@@ -1,7 +1,7 @@
 package io.petproject.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
@@ -9,23 +9,39 @@ import org.junit.jupiter.api.TestInstance
 internal class ShoppingCartTest {
 
     private val shoppingCart = ShoppingCart()
+    private lateinit var account: Account
 
-    @BeforeAll
+    @BeforeEach
     fun `setup`() {
         val console = Product("game console", ProductType.PHYSICAL, 1899.00)
-        val netflix = Product("netflix voucher", ProductType.DIGITAL, 25.00)
-        val amazonPrime = Product("amazon video prime subscription", ProductType.SUBSCRIPTION, 14.90)
-        val physicalBook = Product("cracking the code interview", ProductType.PHYSICAL_TAX_FREE, 219.57)
+        val chair = Product("PDP Chair", ProductType.PHYSICAL, 399.00)
+        val netflix = Product("netflix familiar plan", ProductType.SUBSCRIPTION, 29.90)
+        val spotify = Product("spotify premium", ProductType.SUBSCRIPTION, 14.90)
+        val book = Product("Cracking the Code Interview", ProductType.PHYSICAL_TAX_FREE, 219.57)
+        val anotherBook = Product("The Hitchhiker's Guide to the Galaxy", ProductType.PHYSICAL_TAX_FREE, 120.00)
+        val musicDigitalAlbum = Product("Stairway to Heaven", ProductType.DIGITAL, 5.00)
+        val videoGameDigitalCopy = Product("Nier:Automata", ProductType.DIGITAL, 129.90)
 
         shoppingCart.addItem(Item(console, 1))
-                .addItem(Item(netflix, 4))
-                .addItem(Item(amazonPrime, 1))
-                .addItem(Item(physicalBook, 2))
+                .addItem(Item(chair, 2))
+                .addItem(Item(netflix, 1))
+                .addItem(Item(spotify, 1))
+                .addItem(Item(book, 2))
+                .addItem(Item(anotherBook, 1))
+                .addItem(Item(musicDigitalAlbum, 1))
+                .addItem(Item(videoGameDigitalCopy, 4))
+
+        account = Account("email@domain.suffix", Address.Builder().country("Brazil")
+                .city("Sao Paulo")
+                .state("SP")
+                .zipCode("01000-000")
+                .streetName("Av Paulista, 1000")
+                .build())
     }
 
     @Test
     fun `should compute subtotal of all items in the cart`() {
-        assertThat(shoppingCart.computeSubtotal().toPlainString()).isEqualTo("2453.04")
+        assertThat(shoppingCart.computeSubtotal().toPlainString()).isEqualTo("3825.54")
     }
 
     @Test
