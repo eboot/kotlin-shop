2ff5c62ec961aa0cedcf859e51c912816196898e
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-29 02:20:48 -0300

    Implementing logic to prevent an Order with mixed up Items

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -5,4 +5,9 @@ class Order(val items: List<Item>, val account: Account, val payment: Payment) {
     lateinit var billingAddress: Address
     lateinit var shippingAddress: Address
 
+    init {
+        val productTypes = items.asSequence().map { item -> item.product.type }.toHashSet()
+        check(productTypes.size == 1) { "Items must belong to the same Product Type" }
+    }
+
 }
