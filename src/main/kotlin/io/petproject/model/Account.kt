1767eb8c836aea584144e56da11b907ff75ca903
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 20:37:43 -0300

    Adding Wallet and PaymentMethod operations

--- src/main/kotlin/io/petproject/model/Account.kt
+++ src/main/kotlin/io/petproject/model/Account.kt
@@ -1,3 +1,23 @@
 package io.petproject.model
 
-data class Account(val email: String, val address: Address)
+data class Account(val email: String, val address: Address) {
+
+    var wallet: ArrayList<PaymentMethod> = ArrayList()
+
+    fun addPaymentMethod(paymentMethod: PaymentMethod) {
+        wallet.add(paymentMethod)
+    }
+
+    fun deletePaymentMethod(paymentMethod: PaymentMethod) {
+        wallet.remove(paymentMethod)
+    }
+
+    fun getAllPaymentMethods(): List<PaymentMethod> {
+        return wallet
+    }
+
+    fun getDefaultPaymentMethod(): PaymentMethod? {
+        return if (wallet.isNotEmpty()) wallet[0] else null
+    }
+
+}
