7a504197ca75c3d6f01d548ff4305dcfd0097fd7
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 19:30:09 -0300

    Renaming OrderIface to Order

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -2,7 +2,7 @@ package io.petproject.model
 
 import java.math.BigDecimal
 
-interface OrderIface {
+interface Order {
 
     val items: List<Item>
     val account: Account
@@ -21,7 +21,7 @@ interface OrderIface {
 
 class PhysicalOrder(override val items: List<Item>,
                     override val account: Account,
-                    override val paymentMethod: PaymentMethod) : OrderIface {
+                    override val paymentMethod: PaymentMethod) : Order {
 
     override var status: OrderStatus = OrderStatus.PENDING
     var shippingAddress: Address? = null
@@ -61,7 +61,7 @@ class PhysicalOrder(override val items: List<Item>,
 
 class DigitalOrder(override val items: List<Item>,
                    override val account: Account,
-                   override val paymentMethod: PaymentMethod) : OrderIface {
+                   override val paymentMethod: PaymentMethod) : Order {
 
     override var status: OrderStatus = OrderStatus.PENDING
 
@@ -78,65 +78,12 @@ class DigitalOrder(override val items: List<Item>,
 
 class MembershipOrder(override val items: List<Item>,
                       override val account: Account,
-                      override val paymentMethod: PaymentMethod) : OrderIface {
+                      override val paymentMethod: PaymentMethod) : Order {
 
     override var status: OrderStatus = OrderStatus.PENDING
 
     override fun place() {
         //TODO("not implemented")
-    }
-
-}
-
-
-class Order(val items: List<Item>, val account: Account) {
-
-    val type: Type
-    var shippingAddress: Address? = null
-    var shipments: List<Package>? = null
-    var paymentMethod: PaymentMethod? = null
-    var status: OrderStatus? = null
-
-    constructor(item: Item, account: Account) : this(listOf(item), account)
-
-    init {
-        require(items.isNotEmpty()) { "There must be at least one item to place the Order" }
-        check(items.asSequence()
-                .map { item -> item.group }
-                .toHashSet().size == 1) { "Items must belong to the same Item Group" }
-        type = items[0].group
-    }
-
-    fun shippingAddress(address: Address): Order {
-        this.shippingAddress = address
-        return this
-    }
-
-    fun paymentMethod(paymentMethod: PaymentMethod): Order {
-        this.paymentMethod = paymentMethod
-        return this
-    }
-
-    fun place(): Order {
-        if (type == Type.PHYSICAL) {
-            checkNotNull(shippingAddress) { "Shipping Address must be informed for Orders with physical delivery" }
-            this.shipments = setupPackages()
-        }
-        checkNotNull(paymentMethod) { "A Payment Method must be informed to place the order" }
-        this.status = OrderStatus.PENDING
-        return this //TODO("Persist Order and get the Order ID")
-    }
-
-    private fun setupPackages(): List<Package> {
-        return items.asSequence()
-                .groupBy { item ->
-                    when (item.product.category) {
-                        Category.PHYSICAL_BOOK -> ShippingLabel.TAX_FREE
-                        else -> ShippingLabel.DEFAULT
-                    }
-                }.map { (label, items) ->
-                    Package(items, account.address, label)
-                }.toList()
     }
 
 }
@@ -148,5 +95,3 @@ enum class OrderStatus {
     SHIPPED,
     DELIVERED
 }
-
-
