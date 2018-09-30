b8422c5d0f805ce7b3ee9fadb5d5f6c6dde1ea1b
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 12:37:13 -0300

    Refactoring Order init

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -9,7 +9,9 @@ data class Order(val items: List<Item>, val account: Account) {
     constructor(item: Item, account: Account): this(listOf(item), account)
 
     init {
-        val itemGroup = items.asSequence().map { item -> item.group }.toHashSet()
-        check(itemGroup.size == 1) { "Items must belong to the same Item Group" }
+        require(items.isNotEmpty()) { "There must be at least one item to place the Order" }
+        check(items.asSequence()
+                .map { item -> item.group }
+                .toHashSet().size == 1) { "Items must belong to the same Item Group" }
     }
 }
