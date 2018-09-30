5847f0ba75b98604cb5132cc367e9f9a9de5f2f1
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 12:36:54 -0300

    Refactoring Product/Item tests

--- src/test/kotlin/io/petproject/model/ItemTest.kt
+++ src/test/kotlin/io/petproject/model/ItemTest.kt
@@ -2,51 +2,53 @@ package io.petproject.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.Test
 
 internal class ItemTest {
 
-    @TestFactory
-    fun `Item Tests`() = listOf(
-            DynamicTest.dynamicTest("when quantity is lower than or equalTo 0, throw IllegalArgEx") {
-                assertThrows(IllegalArgumentException::class.java) {
-                    val product = Product("product", Category.PHYSICAL, 1.99)
-                    Item(product, 0)
-                }
-            },
+    @Test
+    fun `when quantity is lower than or equalTo 0, throw IllegalArgEx`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            val product = Product("product", Category.PHYSICAL, 1.99)
+            Item(product, 0)
+        }
+    }
 
-            DynamicTest.dynamicTest("when querying for subtotal, compute unit_price * units") {
-                val product = Product("product", Category.PHYSICAL, 1.99)
-                val item = Item(product, 10)
-                assertThat(item.subtotal.toPlainString()).isEqualTo("19.90")
-            },
+    @Test
+    fun `when querying for subtotal, compute unit_price x units`() {
+        val product = Product("product", Category.PHYSICAL, 1.99)
+        val item = Item(product, 10)
+        assertThat(item.subtotal.toPlainString()).isEqualTo("19.90")
+    }
 
-            DynamicTest.dynamicTest("when product category is a digital copy, item_group must be Digital") {
-                listOf(Category.DIGITAL_MUSIC,
-                        Category.DIGITAL_SOFTWARE,
-                        Category.DIGITAL_COPY_MOVIES_TV,
-                        Category.DIGITAL_VIDEO_GAMES
-                ).map { category ->
-                    Item(Product("product", category, 10.00), 1)
-                }.forEach { item ->
-                    run { assertThat(item.group).isEqualTo(ItemGroup.DIGITAL) }
-                }
-            },
+    @Test
+    fun `when product category is a digital copy, item_group must be Digital`() {
+        listOf(Category.DIGITAL_MUSIC,
+                Category.DIGITAL_SOFTWARE,
+                Category.DIGITAL_COPY_MOVIES_TV,
+                Category.DIGITAL_VIDEO_GAMES
+        ).map { category ->
+            Item(Product("product", category, 10.00), 1)
+        }.forEach { item ->
+            run { assertThat(item.group).isEqualTo(ItemGroup.DIGITAL) }
+        }
+    }
 
-            DynamicTest.dynamicTest("when product category is of physical type, item_group must be Physical") {
-                listOf(Category.PHYSICAL,
-                        Category.PHYSICAL_BOOK
-                ).map { category ->
-                    Item(Product("product", category, 10.00), 1)
-                }.forEach { item ->
-                    run { assertThat(item.group).isEqualTo(ItemGroup.PHYSICAL) }
-                }
-            },
+    @Test
+    fun `when product category is of physical type, item_group must be Physical`() {
+        listOf(Category.PHYSICAL,
+                Category.PHYSICAL_BOOK
+        ).map { category ->
+            Item(Product("product", category, 10.00), 1)
+        }.forEach { item ->
+            run { assertThat(item.group).isEqualTo(ItemGroup.PHYSICAL) }
+        }
+    }
 
-            DynamicTest.dynamicTest("when product category is a subscription, item_group must be Membership") {
-                val item = Item(Product("product", Category.SUBSCRIPTION, 10.00), 1)
-                assertThat(item.group).isEqualTo(ItemGroup.MEMBERSHIP)
-            }
-    )
+    @Test
+    fun `when product category is a subscription, item_group must be Membership`() {
+        val item = Item(Product("product", Category.SUBSCRIPTION, 10.00), 1)
+        assertThat(item.group).isEqualTo(ItemGroup.MEMBERSHIP)
+    }
+
 }
--- src/test/kotlin/io/petproject/model/ProductTest.kt
+++ src/test/kotlin/io/petproject/model/ProductTest.kt
@@ -2,23 +2,28 @@ package io.petproject.model
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.Test
 
 internal class ProductTest {
 
-    @TestFactory
-    fun `Product Tests`() = listOf(
-            DynamicTest.dynamicTest("when name is blank, throw IllegalArgEx") {
-                assertThrows(IllegalArgumentException::class.java) { Product(" ", Category.PHYSICAL, 1.99) }
-            },
-            DynamicTest.dynamicTest("when price is lower than or equalTo 0, throw IllegalArgEx") {
-                assertThrows(IllegalArgumentException::class.java) { Product("product", Category.PHYSICAL, 0.0) }
-            },
-            DynamicTest.dynamicTest("when price has more than 2 digits, round up") {
-                val product = Product("product", Category.PHYSICAL, 1.965)
-                Assertions.assertThat(product.price.toPlainString()).isEqualTo("1.97")
-            }
-    )
+    @Test
+    fun `when name is blank, throw IllegalArgEx`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Product(" ", Category.PHYSICAL, 1.99)
+        }
+    }
+
+    @Test
+    fun `when price is lower than or equalTo 0, throw IllegalArgEx`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Product("product", Category.PHYSICAL, 0.0)
+        }
+    }
+
+    @Test
+    fun `when price has more than 2 digits, round up`() {
+        val product = Product("product", Category.PHYSICAL, 1.965)
+        Assertions.assertThat(product.price.toPlainString()).isEqualTo("1.97")
+    }
 
 }
