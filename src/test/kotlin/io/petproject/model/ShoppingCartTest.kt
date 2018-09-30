1f169cbdc7f4fa8585909d54b6b7c6f218035974
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 12:58:55 -0300

    Updating ShoppingCart checkout for better assertions now that Order has Type

--- src/test/kotlin/io/petproject/model/ShoppingCartTest.kt
+++ src/test/kotlin/io/petproject/model/ShoppingCartTest.kt
@@ -53,7 +53,14 @@ internal class ShoppingCartTest {
 
     @Test
     fun `when checking out, build an Order for Physical, another for Digital, and one Order per Membership`() {
-        val orders: List<Order> = shoppingCart.checkout(account)
+        val orders = shoppingCart.checkout(account)
         assertThat(orders.size).isEqualTo(5)
+
+        val groupedOrders = orders
+                .asSequence()
+                .groupBy { order: Order -> order.type }
+        assertThat(groupedOrders[Type.PHYSICAL]?.size).isEqualTo(1)
+        assertThat(groupedOrders[Type.DIGITAL]?.size).isEqualTo(1)
+        assertThat(groupedOrders[Type.MEMBERSHIP]?.size).isEqualTo(3)
     }
 }
