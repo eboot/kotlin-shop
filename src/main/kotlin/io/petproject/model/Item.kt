e2bdac1c8f10dc3f53bcddd4c119e5e1f264e698
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 11:16:11 -0300

    Adding ItemGroup to lookup ProductCategories into Physical/Digital/Membership

--- src/main/kotlin/io/petproject/model/Item.kt
+++ src/main/kotlin/io/petproject/model/Item.kt
@@ -6,8 +6,19 @@ data class Item(val product: Product, val quantity: Int) {
     val subtotal: BigDecimal = product.price
             .multiply(quantity.toBigDecimal())
 
+    val group: ItemGroup by lazy {
+        when(product.category) {
+            else -> ItemGroup.PHYSICAL
+        }
+    }
+
     init {
         require(quantity > 0) { "Quantity must be > 0" }
     }
 }
 
+enum class ItemGroup {
+    PHYSICAL,
+    DIGITAL,
+    MEMBERSHIP
+}
