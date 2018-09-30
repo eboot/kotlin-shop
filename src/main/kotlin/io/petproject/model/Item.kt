0797391e66addbd04b4847d25bc9e1e22fc0922e
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 11:17:28 -0300

    Implementing digital/membership lookups

--- src/main/kotlin/io/petproject/model/Item.kt
+++ src/main/kotlin/io/petproject/model/Item.kt
@@ -8,6 +8,11 @@ data class Item(val product: Product, val quantity: Int) {
 
     val group: ItemGroup by lazy {
         when(product.category) {
+            Category.DIGITAL_COPY_MOVIES_TV -> ItemGroup.DIGITAL
+            Category.DIGITAL_MUSIC -> ItemGroup.DIGITAL
+            Category.DIGITAL_VIDEO_GAMES -> ItemGroup.DIGITAL
+            Category.DIGITAL_SOFTWARE -> ItemGroup.DIGITAL
+            Category.SUBSCRIPTION -> ItemGroup.MEMBERSHIP
             else -> ItemGroup.PHYSICAL
         }
     }
