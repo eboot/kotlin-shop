4d13aa18aa4dde8bfda8bfa5e7b586832b4abe57
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 23:46:24 -0300

    Including unittests for MembershipOrder for total(), subtotal() and FSM

--- src/test/kotlin/io/petproject/model/MembershipOrderTest.kt
+++ src/test/kotlin/io/petproject/model/MembershipOrderTest.kt
@@ -59,4 +59,59 @@ internal class MembershipOrderTest {
         assertThat(ex.message).isEqualTo("A Payment method must be informed to place the Order")
     }
 
+    @Test
+    fun `when placing a Membership Order, subtotal should be the Item price`() {
+        val order = buildOrder()
+        order.place()
+        assertThat(order.subtotal().toPlainString()).isEqualTo("3256.14")
+    }
+
+    @Test
+    fun `when placing a Membership Order, total should be the Item price`() {
+        val order = buildOrder()
+        order.place()
+        assertThat(order.total().toPlainString()).isEqualTo("3276.14")
+    }
+
+    @Test
+    fun `when paying for Membership Order, throw IllegalStateEx if Status is not PENDING`() {
+        val order = buildOrder()
+        val ex = Assertions.assertThrows(IllegalStateException::class.java) {
+            order.pay()
+        }
+        assertThat(ex.message).isEqualTo("")
+    }
+
+    @Test
+    fun `when paying for Membership Order, Status should be updated to PENDING_ACTIVATION once pay is successful`() {
+        val order = buildOrder()
+        order.place()
+        order.pay()
+        assertThat(order.status).isEqualTo(OrderStatus.PENDING_ACTIVATION)
+    }
+
+    @Test
+    fun `when fulfilling a Membership Order, throw IllegalStateEx if Status is not PAYMENT_COMPLETE`() {
+        val order = buildOrder()
+        order.place()
+        val ex = Assertions.assertThrows(IllegalStateException::class.java) {
+            order.fulfill()
+        }
+        assertThat(ex.message).isEqualTo("")
+    }
+
+    @Test
+    fun `when fulfilling a Membership Order, Status should be updated to ACTIVATED`() {
+        val order = buildOrder()
+        order.place()
+        order.pay()
+        order.fulfill()
+        assertThat(order.status).isEqualTo(OrderStatus.ACTIVATED)
+    }
+
+    private fun buildOrder(): Order {
+        return MembershipOrder(subscriptions, account, null)
+                .paymentMethod(paymentMethod)
+    }
+
 }
