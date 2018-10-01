903fb3940edcefeb8550dc420a6d7a42e998e273
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-30 21:24:06 -0300

    Stubbing shippingCosts for Parcel/Package

--- src/main/kotlin/io/petproject/model/Package.kt
+++ src/main/kotlin/io/petproject/model/Package.kt
@@ -1,5 +1,8 @@
 package io.petproject.model
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 data class Package(val items: List<Item>, val shippingAddress: Address, val shippingLabel: ShippingLabel) {
 
     constructor(items: List<Item>, shippingAddress: Address) : this(items, shippingAddress, ShippingLabel.DEFAULT)
@@ -7,6 +10,13 @@ data class Package(val items: List<Item>, val shippingAddress: Address, val ship
     init {
         require(items.isNotEmpty()) { "There must be at least one item to package" }
     }
+
+    fun getShippingCosts(): BigDecimal {
+        //TODO: Implement service to calc Shipping Costs for this Parcel
+        return BigDecimal.TEN
+                .setScale(2, RoundingMode.HALF_UP)
+    }
+
 }
 
 enum class ShippingLabel(val label: String = "") {
